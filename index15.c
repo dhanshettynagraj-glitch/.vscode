616a46a65bdaef0bde30ebfa799dfcb9f5773c23
dhanshettynagraj-glitch <dhanshettynagraj@gmail.com>
2025-11-11 08:43:23 +0530

    Add files via upload

---
+++ index15.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int a=10,b=4;
+    printf("Addition = %d\n",a+b);
+    printf("Substraction = %d\n",a-b);
+    printf("Multiplication = %d\n",a*b);
+    printf("Division = %d\n",a/b);
+    printf("Modules = %d\n",a%b);
+    return 0;
+}
+
